490b402c5f01e3634229aa75e856f9c9019d7015
Tom Castleman <tom.castleman@b3-it.com>
2013-11-13 03:39:22 +0000

    expose connection for testing
    Signed-off-by: Tom Castleman <tom.castleman@b3-it.com>

--- classes/database/mssql.php
+++ classes/database/mssql.php
@@ -1,3 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Database_MsSQL extends Kohana_Database_MsSQL {}
+class Database_MsSQL extends Kohana_Database_MsSQL
+{
+    public function connection()
+    {
+        // Make sure the database is connected
+        $this->_connection or $this->connect();
+        
+        // Return it
+        return $this->_connection;
+    }
+}
